63d4a9d26ff8d1e4322910014ea4e3961fa31cd7
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 11:48:40 -0300

    Simple ExpenseItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
-function App() {
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ExpenseItem = () => <h2>Expense Item!</h2>;
+
+export default ExpenseItem;
