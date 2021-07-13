beac5c1309d266ae2d829f8d95ffe5b7487c3ab7
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 14:41:17 -0300

    Class 48 - UseState

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; date: Date; }) => {
-  const clickHandler = () => console.log('Clicked!!!');
+  const [currentTitle, setCurrentTitle] = useState(title);
+
+  const clickHandler = () => setCurrentTitle('Updated');
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{currentTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
       <button type="button" onClick={clickHandler}>Change Title</button>
