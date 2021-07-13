ec78c7b1f7e3773b9fb22f6bcdfbcdd9d9ca9064
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 14:58:41 -0300

    Class 51 - NewExpense

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -2,39 +2,21 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-const Expenses = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+interface IExpense {
+  title: string;
+  amount: number;
+  date: Date;
+}
 
-  return (
-    <div className="expenses">
-      {expenses.map((expense) =>
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />)}
-    </div>
-  );
-};
+const Expenses = ({ expenses }: { expenses: IExpense[] }) => (
+  <div className="expenses">
+    {expenses.map((expense) =>
+      <ExpenseItem
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />)}
+  </div>
+);
 
 export default Expenses;
