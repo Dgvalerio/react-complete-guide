f3688075220963c26a9f9a76cc04f2cf6c134fcb
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 17:07:01 -0300

    Class 59 and Filter

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
 
 export interface IExpense {
   id?: string;
@@ -9,15 +10,24 @@ export interface IExpense {
   date: Date;
 }
 
-const Expenses = ({ expenses }: { expenses: IExpense[] }) => (
-  <div className="expenses">
-    {expenses.map((expense) =>
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />)}
-  </div>
-);
+const Expenses = ({ expenses }: { expenses: IExpense[] }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear: string) => setFilteredYear(selectedYear);
+
+  return (
+    <div className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {expenses
+        .filter((item) => item.date.getFullYear() === +filteredYear)
+        .map((expense) =>
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />)}
+    </div>
+  );
+};
 
 export default Expenses;
