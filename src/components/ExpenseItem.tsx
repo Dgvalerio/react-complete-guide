814ceaa86551fb34084e2fcacedc0ab5547511b6
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 12:15:49 -0300

    Aula 37

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; date: Date; }) => (
-  <div className="expense-item">
-    <div>{date.toDateString()}</div>
-    <div className="expense-item__description">
-      <h2>{title}</h2>
-      <div className="expense-item__price">${amount}</div>
+const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; date: Date; }) => {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const year = date.toLocaleString('en-US', { year: '2-digit' });
+  const day = date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ExpenseItem;
