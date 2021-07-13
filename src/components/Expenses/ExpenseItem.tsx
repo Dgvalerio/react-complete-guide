54bced7586c49275781457cc45de5fa4648b9234
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 14:27:13 -0300

    Class 46 - Click Event

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -4,6 +4,8 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; date: Date; }) => {
+  const clickHandler = () => console.log('Clicked!!!');
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -11,6 +13,7 @@ const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; d
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button type="button" onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
