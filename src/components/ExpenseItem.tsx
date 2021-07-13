b71b5ddd811d1a44dcf579b86ebaeedd9f018ea2
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-13 12:11:27 -0300

    Aula 36

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = '294.67';
-
-  return (
-    <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
-      </div>
+const ExpenseItem = ({ title, amount, date }: { title: string; amount: number; date: Date; }) => (
+  <div className="expense-item">
+    <div>{date.toDateString()}</div>
+    <div className="expense-item__description">
+      <h2>{title}</h2>
+      <div className="expense-item__price">${amount}</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ExpenseItem;
